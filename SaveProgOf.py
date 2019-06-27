259afaa4da50e42e4bafc41190fa05567cd5f129
Raylex Lee <raylexym57lee@gmail.com>
2019-06-27 17:07:54 +0800

    Update for new version

--- SaveProgOf.py
+++ SaveProgOf.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-from urllib.request  import urlopen  
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+from lxml import etree
 import os, sys
-import re
 import pickle
-# culturalPodcastsUrl = 'http://podcast.rthk.hk/podcast/list_category.php?lang=zh-CN&action=do_rthk_cat&rthk_cat=%E6%96%87%E5%8C%96'
-# html = urlopen(culturalPodcastsUrl)
-allPodcastsUrl = 'http://podcast.rthk.hk/podcast/list_all.php?lang=zh-CN'
-html = urlopen(allPodcastsUrl)
-bsObj = BeautifulSoup(html,"lxml")
-podcastsLinks = bsObj.findAll("a",{"href":re.compile("item_epi")})
+import requests
+def allCulturalPodcastURL(page):
+    return "https://podcast.rthk.hk/podcast/programmeList.php?type=audio&cid=0&page="+page+"&order=stroke&lang=zh-CN"
+page = 0
+remainder = '99'
 ProgOf = {}
-for podcastsLink in podcastsLinks:
-    id = podcastsLink["href"][17:]
-    title = podcastsLink.find("div",{"class":"prog-title"})
-    if title.span["class"][0] == "icon-audio":
-        ProgOf[id] = title.get_text()
+while (remainder != '0'):
+    page = page + 1
+    html = requests.get(allCulturalPodcastURL(str(page)))
+    root = etree.fromstring(html.content)
+    remainder = root.xpath('/programmeList/remainder')[0].text
+    for programme in root.xpath('//programme'):
+         title = programme.xpath('title')[0].text
+         link = programme.xpath('link')[0].text
+         x = link.split('=')
+         id = x[1]
+         ProgOf[id] = title
+#print(ProgOf)         
 pickle_out = open("ProgOf.pickle","wb")
 pickle.dump(ProgOf, pickle_out)
 pickle_out.close()        
